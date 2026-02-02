5e96661741524594ca92660e9b80b1cc1298f9e8
Guilherme Cristaldo <guilherme.cristaldo15@gmail.com>
2026-02-02 08:02:51 -0300

    Refactor controller to handle requisition form POST

--- obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5900bd65edde49cc3ccc374bf63ab5b434940aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
